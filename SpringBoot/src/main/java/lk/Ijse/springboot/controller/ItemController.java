7be8b3df3e8def168af4c2823988370998d8d725
dvish2003 <Vishanchathuranga2003@gmail.com>
2025-02-19 00:33:25 +0530

    item and customer crud done

--- SpringBoot/src/main/java/lk/Ijse/springboot/controller/ItemController.java
+++ SpringBoot/src/main/java/lk/Ijse/springboot/controller/ItemController.java
@@ -1,4 +1,50 @@
 package lk.Ijse.springboot.controller;
 
+import lk.Ijse.springboot.dto.CustomerDTO;
+import lk.Ijse.springboot.dto.ItemDTO;
+import lk.Ijse.springboot.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/item")
 public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/get")
+    public List<ItemDTO> getItem(){
+        List<ItemDTO> item = itemService.getAllItem();
+        return item;
+    }
+    @PostMapping("/save")
+    public boolean save(@RequestBody ItemDTO itemDTO){
+        boolean resp = itemService.saveItem(itemDTO);
+        if (resp){
+            System.out.println("Item saved successfully");
+            return resp;
+        }
+        return false;
+    }
+    @PutMapping("/update")
+    public boolean Update(@RequestBody  ItemDTO itemDTO){
+        boolean resp = itemService.UpdateItem(itemDTO);
+        if(resp){
+            System.out.println("Item updated successfully");
+            return resp;
+        }
+        return false;
+    }
+    @DeleteMapping("/delete")
+    public boolean delete(@PathVariable  String id){
+        boolean resp = itemService.DeleteItem(id);
+        if (resp){
+            System.out.println("Item deleted");
+            return resp;
+
+        }
+        return false;
+    }
 }
--- SpringBoot/src/main/java/lk/Ijse/springboot/service/ItemService.java
+++ SpringBoot/src/main/java/lk/Ijse/springboot/service/ItemService.java
@@ -1,8 +1,43 @@
 package lk.Ijse.springboot.service;
 
+import lk.Ijse.springboot.dto.ItemDTO;
+import lk.Ijse.springboot.entity.Item;
+import lk.Ijse.springboot.repo.ItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemService {
+    @Autowired
+private ItemRepo itemRepo;
+    public List<ItemDTO> getAllItem() {
+        List<Item> items = itemRepo.findAll();
+        return items.stream().map(item -> new ItemDTO(item.getId(), item.getName(), item.getPrice(), item.getQty())).collect(Collectors.toList());
+    }
 
+    public boolean saveItem(ItemDTO itemDTO) {
+        Item item = new Item(itemDTO.getId(), itemDTO.getName(), itemDTO.getPrice(),itemDTO.getQty());
+        itemRepo.save(item);
+        return true;
+    }
+
+    public boolean UpdateItem(ItemDTO itemDTO) {
+        if(itemRepo.existsById(itemDTO.getId())){
+            Item item = new Item(itemDTO.getId(), itemDTO.getName(), itemDTO.getPrice(),itemDTO.getQty());
+            itemRepo.save(item);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean DeleteItem(String id) {
+        if(itemRepo.existsById(Integer.valueOf(id))){
+            itemRepo.deleteById(Integer.parseInt(id));
+            return true;
+        }
+        return false;
+    }
 }
