3799c965c6a8fd81f96a39be8bd04a70a5f0ce82
dvish2003 <Vishanchathuranga2003@gmail.com>
2025-02-22 00:53:19 +0530

    customer crud done use frontEnd

--- SpringBoot/src/main/java/lk/Ijse/springboot/controller/CustomerController.java
+++ SpringBoot/src/main/java/lk/Ijse/springboot/controller/CustomerController.java
@@ -2,49 +2,129 @@
 package lk.Ijse.springboot.controller;
 
 import lk.Ijse.springboot.dto.CustomerDTO;
+import lk.Ijse.springboot.dto.ResponseDTO;
 import lk.Ijse.springboot.service.CustomerService;
+import lk.Ijse.springboot.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/customer")
+@CrossOrigin
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
+    @Autowired
+    private ResponseDTO responseDTO;
     @GetMapping("/get")
-    public List<CustomerDTO> getCustomer(){
-        List<CustomerDTO> customers = customerService.getAllEmployee();
-        return customers;
+    public ResponseEntity getCustomer(){
+        try {
+            List<CustomerDTO> customerDTOList = customerService.getAll();
+            responseDTO.setCode(VarList.RSP_SUCCESS);
+            responseDTO.setMessage("Success");
+            responseDTO.setContent(customerDTOList);
+            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+        }catch (Exception ex){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
     }
     @PostMapping("/save")
-    public boolean saveCustomer(@RequestBody  CustomerDTO customerDTO){
-       boolean resp = customerService.saveCustomer(customerDTO);
-       if (resp){
-           System.out.println("Customer saved successfully");
-           return resp;
-       }
-       return false;
+    public ResponseEntity saveCustomer(@RequestBody  CustomerDTO customerDTO){
+        try {
+        String res=customerService.saveCustomer(customerDTO);
+
+        if(res.equals("00")){
+            responseDTO.setCode(VarList.RSP_SUCCESS);
+            responseDTO.setMessage("Success");
+            responseDTO.setContent(customerDTO);
+            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+        }else if(res.equals("06")) {
+
+            responseDTO.setCode(VarList.RSP_DUPLICATED);
+            responseDTO.setMessage("Customer Registered");
+            responseDTO.setContent(customerDTO);
+            return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+        }else {
+            responseDTO.setCode(VarList.RSP_FAIL);
+            responseDTO.setMessage("Error");
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+        }
+
+    }catch (Exception ex){
+
+        responseDTO.setCode(VarList.RSP_ERROR);
+        responseDTO.setMessage(ex.getMessage());
+        responseDTO.setContent(null);
+        return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
     }
  @PutMapping("/update")
-    public boolean Update(@RequestBody  CustomerDTO customerDTO){
-       boolean resp = customerService.updateCustomer(customerDTO);
-       if(resp){
-           System.out.println("Customer updated successfully");
-           return resp;
-       }
-       return false;
-    }
- @DeleteMapping("/delete")
-    public boolean delete(@PathVariable  String id){
-       boolean resp = customerService.DeleteCustomer(id);
-       if (resp){
-           System.out.println("Customer deleted");
-           return resp;
+    public ResponseEntity Update(@RequestBody  CustomerDTO customerDTO){
+     try {
+         String res=customerService.updateCustomer(customerDTO);
 
-       }
-       return false;
+         if(res.equals("00")){
+             responseDTO.setCode(VarList.RSP_SUCCESS);
+             responseDTO.setMessage("Success");
+             responseDTO.setContent(customerDTO);
+             return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+         }else if(res.equals("06")) {
+
+             responseDTO.setCode(VarList.RSP_DUPLICATED);
+             responseDTO.setMessage("Customer Registered");
+             responseDTO.setContent(customerDTO);
+             return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+         }else {
+             responseDTO.setCode(VarList.RSP_FAIL);
+             responseDTO.setMessage("Error");
+             responseDTO.setContent(null);
+             return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+         }
+
+     }catch (Exception ex){
+
+         responseDTO.setCode(VarList.RSP_ERROR);
+         responseDTO.setMessage(ex.getMessage());
+         responseDTO.setContent(null);
+         return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+
+     }
+    }
+ @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable  String id){
+     try {
+         String res = customerService.DeleteCustomer(id);
+         if (res.equals("00")) {
+             responseDTO.setCode(VarList.RSP_SUCCESS);
+             responseDTO.setMessage("Success");
+             responseDTO.setContent(null);
+             return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+         } else {
+             responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
+             responseDTO.setMessage("No Customer Available For this ID");
+             responseDTO.setContent(null);
+             return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+         }
+     } catch (Exception e) {
+         responseDTO.setCode(VarList.RSP_ERROR);
+         responseDTO.setMessage(e.getMessage());
+         responseDTO.setContent(e);
+         return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+     }
     }
 
 
--- SpringBoot/src/main/java/lk/Ijse/springboot/service/CustomerService.java
+++ SpringBoot/src/main/java/lk/Ijse/springboot/service/CustomerService.java
@@ -3,45 +3,55 @@ package lk.Ijse.springboot.service;
 import lk.Ijse.springboot.dto.CustomerDTO;
 import lk.Ijse.springboot.entity.Customer;
 import lk.Ijse.springboot.repo.CustomerRepo;
+import lk.Ijse.springboot.util.VarList;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Service  // service eke service annotation ek aniwaren dnn oni
+@Transactional
 public class CustomerService {
 @Autowired
     private CustomerRepo customerRepo;
-    public Boolean saveCustomer(CustomerDTO customerDTO){
-        Customer customer = new Customer(customerDTO.getId(), customerDTO.getName(), customerDTO.getAddress());
-        customerRepo.save(customer);
-        return true;
-
-    }
-    public List<CustomerDTO> getAllEmployee(){
-        List<Customer> employeeList = customerRepo.findAll();
-        List<CustomerDTO> customerDTOS = new ArrayList<>();
-        for (Customer customerDTO : employeeList){
-            CustomerDTO customerDTO1 = new CustomerDTO(customerDTO.getId(), customerDTO.getName(), customerDTO.getAddress());
-            customerDTOS.add(customerDTO1);
+@Autowired
+    private ModelMapper modelMapper;
+    public String saveCustomer(CustomerDTO customerDTO){
+        if (customerRepo.existsById(customerDTO.getId())){
+            return VarList.RSP_DUPLICATED;
+        }else {
+            customerRepo.save(modelMapper.map(customerDTO, Customer.class));
+            return VarList.RSP_SUCCESS;
         }
-        return customerDTOS;
+
+    }
+    public List<CustomerDTO> getAll(){
+        List<Customer> customerList = customerRepo.findAll();
+        return modelMapper.map(customerList,new TypeToken<ArrayList<CustomerDTO>>(){
+        }.getType());
     }
 
-    public boolean updateCustomer(CustomerDTO customerDTO) {
-        if(customerRepo.existsById(customerDTO.getId())){
-            Customer customer = new Customer(customerDTO.getId(), customerDTO.getName(), customerDTO.getAddress());
-            customerRepo.save(customer);
+    public String updateCustomer(CustomerDTO customerDTO) {
+        if (customerRepo.existsById(customerDTO.getId())){
+            customerRepo.save(modelMapper.map(customerDTO, Customer.class));
+            return VarList.RSP_SUCCESS;
+
+        }else {
+            return VarList.RSP_NO_DATA_FOUND;
         }
-        return true;
     }
 
-    public boolean DeleteCustomer(String id) {
-if(customerRepo.existsById(Integer.valueOf(id))){
-    customerRepo.deleteById(Integer.parseInt(id));
-
-}        return true;
+    public String DeleteCustomer(String id) {
+        if (customerRepo.existsById(Integer.valueOf(id))){
+            customerRepo.deleteById(Integer.valueOf(id));
+            return VarList.RSP_SUCCESS;
+        }else {
+            return VarList.RSP_NO_DATA_FOUND;
+        }
     }
 }
